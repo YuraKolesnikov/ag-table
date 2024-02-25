8bffdd0a6e476c0011e29ac09eb4724b6c9d1661
YuryKolesnikov <jurijs.kolesnikovs@gmail.com>
2024-02-25 12:06:37 +0700

    Added global stylesheet import

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'app/scss/main.scss'
 
 import App from 'app/app'
 
-ReactDOM.render(React.createElement(App), document.getElementById('app'));
+ReactDOM.render(React.createElement(App), document.getElementById('app'))
